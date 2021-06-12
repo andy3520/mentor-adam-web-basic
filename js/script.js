6b91cb3153054c55fec82d3d902be68478c541ba
An Nguyen <andy.annguyen3520@gmail.com>
2021-06-12 17:47:08 +0700

    finish

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
-let a = 1;
+var numberAElement = document.getElementById("numberA");
+var numberBElement = document.getElementById("numberB");
+var resultElement = document.getElementById("result");
+
+function sumNumber() {
+  var numberA = parseInt(numberAElement.value);
+  var numberB = parseInt(numberBElement.value);
+  
+  var result = numberA + numberB;
+
+  resultElement.innerText = "Result is:" + result;
+}
